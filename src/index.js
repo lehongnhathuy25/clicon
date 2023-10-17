342b743827af1929ffa43ae51092c1421432cac1
Your Name <you@example.com>
2023-10-17 13:14:11 +0700

    setup project

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ const express = require('express') ;
 const { engine } = require ('express-handlebars') ;
 const path = require('path');
 const app = express();
-
 app.engine('hbs', engine({extname: '.hbs'}));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname,'resources/views'));
-
 app.get('/', (req, res) => {
     res.render('home');
 });
