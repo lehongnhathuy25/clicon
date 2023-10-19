5a88352d862da20c0b9cb54bcbc64bd90408ba7d
Your Name <you@example.com>
2023-10-19 15:13:47 +0700

    ui header

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.engine('hbs', engine({extname: '.hbs'}));
 app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname,'resources/views'));
 
 app.get('/', (req, res) => {
